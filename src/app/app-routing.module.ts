79999d36f968aa5bf6b250b2e681bd629ed4343a
GiuseppeLanucara <giuseppelanucara96@gmail.com>
2023-03-13 20:03:45 +0100

    Router outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { LoginComponent } from './login/login.component';
 import { MovieComponent } from './movie/movie.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' }
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent },
   { path: 'movie', component: MovieComponent },
